d06741542ab17919a1bd547c609dee21ef27a957
James Peach <james_peach01@hotmail.co.uk>
2018-02-04 13:28:37 +0000

    fixed codegen bug

--- src/codegen_x86.h
+++ src/codegen_x86.h
@@ -9,52 +9,47 @@ class StreamL1 {
 
 public:
   StreamL1() {}
-  void assignAcc(const int v) { out << "  mov eax, " << v << '\n'; }
+  void assignAcc(const int v) { out << "  mov rax, " << v << '\n'; }
 
   void assignAccMem(int o)
   {
-    out << "  mov eax, DWORD PTR [rbp-" << o * 4 << "] " << '\n';
+    out << "  mov rax, DWORD PTR [rbp-" << o * 4 << "] " << '\n';
   }
 
-  void push() { out << "  push eax\n"; }
+  void push() { out << "  pushq rax\n"; }
 
-  void pop() { out << "  pop eax\n"; }
+  void popTmp() { out << "  popq rdx\n"; }
 
-  void saveTmp() { out << "  mov edx, eax\n"; }
+  void saveTmp() { out << "  mov rdx, rax\n"; }
 
-  void saveFp() { out << "  push rbp\n"; }
-
-  void restoreFp() { out << "  pop rbp\n"; }
-
-  void setupStack() { out << "  mov rbp, rsp\n"; }
-
-  void pushn(int n)
-  {
-    (void)n;
-    return;
+  void setupStack(int vars) 
+  { 
+    out << "  push rbp\n";
+    out << "  mov rbp, rsp\n"; 
+    out << "  sub rsp, " << vars * 4 << '\n';
   }
 
-  void popn(int n)
+  void restoreStack() 
   {
-    (void)n;
-    return;
+    out << "  mov rbp, rsp\n"; 
+    out << "  pop rbp\n";
   }
 
   void storeMem(int o)
   {
-    out << "  mov DWORD PTR [rbp-" << o * 4 << "], eax" << '\n';
+    out << "  mov DWORD PTR [rbp-" << o * 4 << "], rax" << '\n';
   }
 
-  void addTmp() { out << "  add eax, edx\n"; }
-  void minusTmp() { out << "  sub eax, edx\n"; }
-  void multiplyTmp() { out << "  imul eax, edx\n"; }
-  void divideTmp() { out << "  idiv eax, edx\n"; }
-  void swap() { out << "  xchg eax,edx\n"; }
+  void addTmp() { out << "  add rax, rdx\n"; }
+  void minusTmp() { out << "  sub rax, rdx\n"; }
+  void multiplyTmp() { out << "  imul rax, rdx\n"; }
+  void divideTmp() { out << "  idiv rax, rdx\n"; }
+  void swap() { out << "  xchg rax, rdx\n"; }
   void ret() { out << "  ret" << '\n'; }
 
   void jumpIfZero(const std::string &label)
   {
-    out << "  cmp eax, 0\n";
+    out << "  cmp rax, 0\n";
     out << "  jz " << label << '\n';
   }
   void jump(const std::string &label) { out << "  jmp " << label << '\n'; }
